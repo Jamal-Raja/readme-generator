976252efd52bd5ed14dd6caaa7f1a353bfb582f4
Jamal Raja <Jamalraja083@gmail.com>
2025-10-21 23:49:18 +0100

    Add multiline input function and update README generator logic

---
+++ functions.py
@@ -0,0 +1,15 @@
+# ============ ALL FUNCTIONS ============
+def multiline_input(prompt):
+    print(f"{prompt} (press Enter twice to finish):")
+    lines = []
+    while True:
+        line = input()
+        if not line:
+            break
+        lines.append(line)
+    output = "\n".join(lines)    
+    return output
+
+multiline_input("Enter a description")
+
+
--- readme_generator.py
+++ readme_generator.py
@@ -1,10 +1,27 @@
+from art import * 
+from rich import print
+from rich.console import Console
+from functions import *
+
 # ============ README GENERATOR ============
-import numpy as np
+console = Console()
 
-print(f'NumPY Version: {np.__version__}')
-print("Welcome to Jamal's Readme Generator \n")
+ascii_art = text2art("Readme Generator", font="small")
+console.print(ascii_art, style="bright_magenta")
 
-def create_readme ():
+# console.print("Hiking", style="bold red")
+
+data_dict = {
+    "title": "",
+    "description": "",
+    "installation_instructions": "",
+    "features": "",
+    "tech_stack": "",    
+}
+
+
+
+def create_readme ():    
     name = input('Before we can begin, please enter your name: ')
     print (f'welcome {name}, I just wanted to say you have a awesome name!')
 
@@ -13,7 +30,16 @@ def create_readme ():
     installation_instructions = input(f'Enter installation instructions for your {title} project: ')
     features = input(f'Enter features of your {title} project: ')
     tech_stack = input(f'Enter tech stack used to build {title}: ')
-    return 
+
+    data_dict.update({
+        "title": title,
+        "description": description,
+        "installation_instructions": installation_instructions,
+        "features": features,
+        "tech_stack": tech_stack,
+    })
+    
+    return print(data_dict)
 
 
 create_readme()
